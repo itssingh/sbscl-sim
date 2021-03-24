f5acebbdc9d547cdda973597960a9a15a5e01ef4
Avinal Kumar <avinal.xlvii@gmail.com>
2021-03-24 18:34:51 +0530

    test 8

--- libc.sh
+++ libc.sh
@@ -14,7 +14,7 @@ export PATH="${METACALL_PATH}/bin:/bin:/usr/bin:/sbin:/usr/sbin"
 
 mkdir -p "${METACALL_PATH}/libc"
 pwd
-
+ls -a
 curl https://ftp.gnu.org/gnu/glibc/glibc-${METACALL_GLIBC_VERSION}.tar.bz2 --output glibc.tar.bz2
 curl https://ftp.gnu.org/gnu/glibc/glibc-${METACALL_GLIBC_VERSION}.tar.bz2.sig --output glibc.tar.bz2.sig
 gpg --verify glibc.tar.bz2.sig glibc.tar.bz2
@@ -23,13 +23,14 @@ mkdir -p "glibc-${METACALL_GLIBC_VERSION}/build"
 cd "glibc-${METACALL_GLIBC_VERSION}/build"
 pwd
 ../configure
-		--prefix="${METACALL_PATH}/libc" \
+		--prefix=../../metacall/libc \
 		--host=${METACALL_ARCH_HOST} \
 		--build=$(../scripts/config.guess) \
 		--enable-kernel=3.2 \
 		libc_cv_forced_unwind=yes \
 		libc_cv_c_cleanup=yes \
 make -j $(nproc)
+
 sudo make install
 cd ../..
 sudo rm -rf glibc-${METACALL_GLIBC_VERSION} glibc.tar.bz2 glibc.tar.bz2.sig
